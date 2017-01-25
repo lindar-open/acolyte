812aa39d520bd62c7582a28c800b18c3df0a1fc8
David Sharp <david@lindar.com>
2017-01-25 14:00:24 +0000

    Added MessageVO to allow all services to use messages.

---
+++ src/main/java/lindar/acolyte/vo/MessageVO.java
@@ -0,0 +1,15 @@
+package lindar.acolyte.vo;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class MessageVO {
+    
+    private String username;
+    
+    private String body;
+    
+    private Date createdAt;
+    
+}
