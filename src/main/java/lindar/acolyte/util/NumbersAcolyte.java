612d5c5da33824b68d4102893ac9c4b49164e54b
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-05-31 19:53:41 +0100

    add equality methods to NumbersAcolyte

--- src/main/java/lindar/acolyte/util/NumbersAcolyte.java
+++ src/main/java/lindar/acolyte/util/NumbersAcolyte.java
@@ -14,6 +14,49 @@ public class NumbersAcolyte {
     }
     
     
+    public static boolean nullOrEqual(Number initialNumber, Number comparingNumber) {
+        if (initialNumber == null && comparingNumber == null) {
+            return true;
+        }
+        double initial = 0d;
+        if (initialNumber != null) {
+            initial = initialNumber.doubleValue();
+        }
+        double comparing = 0d;
+        if (comparingNumber != null) {
+            comparing = comparingNumber.doubleValue();
+        }
+        return initial == comparing;
+    }
+    
+    /**
+     * The numbers have to be NON-NULL. Otherwise use the nullOrEqual method
+     * @param initialNumber
+     * @param comparingNumber
+     * @return
+     */
+    public static boolean equal(Number initialNumber, Number comparingNumber) {
+        return initialNumber != null && comparingNumber != null 
+                && initialNumber.doubleValue() == comparingNumber.doubleValue();
+    }
+    
+    public static boolean notEqualAndNotNull(Number initialNumber, Number comparingNumber) {
+        return initialNumber != null && comparingNumber != null 
+                && initialNumber.doubleValue() != comparingNumber.doubleValue();
+    }
+    
+    public static boolean notEqual(Number initialNumber, Number comparingNumber) {
+        double initial = 0d;
+        if (initialNumber != null) {
+            initial = initialNumber.doubleValue();
+        }
+        double comparing = 0d;
+        if (comparingNumber != null) {
+            comparing = comparingNumber.doubleValue();
+        }
+        return initial != comparing;
+    }
+    
     public static boolean greaterThanZero(Number number) {
         return number != null && number.doubleValue() > 0d;
     }
@@ -41,6 +84,10 @@ public class NumbersAcolyte {
     
     public static boolean lessThan(Number initialNumber, Number comparingNumber) {
         return initialNumber != null && initialNumber.doubleValue() < comparingNumber.doubleValue();
+    }
+    
+    public static boolean nullOrLessThan(Number initialNumber, Number comparingNumber) {
+        return initialNumber == null || initialNumber.doubleValue() < comparingNumber.doubleValue();
     }
     
     public static boolean equalOrLessThan(Number initialNumber, Number comparingNumber) {
