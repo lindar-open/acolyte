21827c8d81d30586eaae7357b7138c52c15a7715
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-07-31 09:53:22 +0100

    update dependencies and add PaginationVO

--- src/main/kotlin/lindar/acolyte/vo/pagination.kt
+++ src/main/kotlin/lindar/acolyte/vo/pagination.kt
@@ -1,5 +1,38 @@
 package lindar.acolyte.vo
 
+data class PageableCollection<T> (val contents: List<T>, val pagination: PaginationVO, val sort: SortVO)
+
+data class PaginationVO (val page: Int, val size: Int, val totalPages: Int, val totalElements: Long) {
+    companion object Builder {
+        private var page = 0
+        private var size = 20
+        private var totalPages = 0
+        private var totalElements = 0L
+
+        fun page(page: Int): PaginationVO.Builder {
+            this.page = page
+            return this
+        }
+
+        fun size(size: Int): PaginationVO.Builder {
+            this.size = size
+            return this
+        }
+
+        fun totalPages(totalPages: Int): PaginationVO.Builder {
+            this.totalPages = totalPages
+            return this
+        }
+
+        fun totalElements(totalElements: Long): PaginationVO.Builder {
+            this.totalElements = totalElements
+            return this
+        }
+
+
+        fun build() = PaginationVO(page, size, totalPages, totalElements)
+    }
+}
 
 data class PageableVO (val page: Int, val size: Int, val sort: List<SortVO>? = null) {
     companion object Builder {
