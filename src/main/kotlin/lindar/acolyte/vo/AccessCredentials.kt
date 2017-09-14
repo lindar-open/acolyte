1d090abd991950e8e4332a74d702208c1c196ef4
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-09-14 10:49:02 +0100

    improve AccessCredentials

--- src/main/kotlin/lindar/acolyte/vo/AccessCredentials.kt
+++ src/main/kotlin/lindar/acolyte/vo/AccessCredentials.kt
@@ -2,11 +2,21 @@ package lindar.acolyte.vo
 
 
 data class AccessCredentials (
-        val apiUrl: String = "",
-        val sessionCookie: String = "",
+        val apiUrl: String,
         val username: String = "",
-        val password: String = ""
+        val password: String = "",
+        val sessionCookie: String = "",
+        val authHeader: String = ""
 ) {
-    constructor(apiUrl: String, username: String, password: String) : this(apiUrl, "", username, password)
-    constructor(apiUrl: String, sessionCookie: String) : this(apiUrl, sessionCookie, "", "")
+    companion object {
+        fun withUsernameAndPassword(apiUrl: String, username: String, password: String): AccessCredentials {
+            return AccessCredentials(apiUrl = apiUrl, username = username, password = password)
+        }
+        fun withAuthHeader(apiUrl: String, authHeader: String): AccessCredentials {
+            return AccessCredentials(apiUrl = apiUrl, authHeader = authHeader)
+        }
+        fun withSessionCookie(apiUrl: String, sessionCookie: String): AccessCredentials {
+            return AccessCredentials(apiUrl = apiUrl, sessionCookie = sessionCookie)
+        }
+    }
 }
