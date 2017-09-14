82742f59cd61e1c4a14b3bb3643dc0d83b3288da
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-09-14 21:24:35 +0100

    add @JvmStatic for AccessCredentials companion functions, release 1.3.1

--- src/main/kotlin/lindar/acolyte/vo/AccessCredentials.kt
+++ src/main/kotlin/lindar/acolyte/vo/AccessCredentials.kt
@@ -9,12 +9,15 @@ data class AccessCredentials (
         val authHeader: String = ""
 ) {
     companion object {
+        @JvmStatic
         fun withUsernameAndPassword(apiUrl: String, username: String, password: String): AccessCredentials {
             return AccessCredentials(apiUrl = apiUrl, username = username, password = password)
         }
+        @JvmStatic
         fun withAuthHeader(apiUrl: String, authHeader: String): AccessCredentials {
             return AccessCredentials(apiUrl = apiUrl, authHeader = authHeader)
         }
+        @JvmStatic
         fun withSessionCookie(apiUrl: String, sessionCookie: String): AccessCredentials {
             return AccessCredentials(apiUrl = apiUrl, sessionCookie = sessionCookie)
         }
