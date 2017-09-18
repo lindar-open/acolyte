c978653e240cf3eb1857a86ec5ae5c18462353ff
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-09-18 09:23:01 +0100

    replace and add path params

--- src/main/kotlin/lindar/acolyte/util/UrlAcolyte.kt
+++ src/main/kotlin/lindar/acolyte/util/UrlAcolyte.kt
@@ -6,16 +6,25 @@ import lindar.acolyte.vo.SortVO
 
 class UrlAcolyte {
     companion object {
-        private val PAGE_PARAM = "page"
-        private val SIZE_PARAM = "size"
-        private val SORT_PARAM = "sort"
+        private const val PAGE_PARAM = "page"
+        private const val SIZE_PARAM = "size"
+        private const val SORT_PARAM = "sort"
 
-        private val AND = "&"
-        private val QUESTION = "?"
-        private val EQUAL = "="
-        private val COMMA = ","
-        private val SLASH = "/"
+        private const val AND = "&"
+        private const val QUESTION = "?"
+        private const val EQUAL = "="
+        private const val COMMA = ","
+        private const val SLASH = "/"
 
+        private const val PATH_PARAM_LEFT = "{"
+        private const val PATH_PARAM_RIGHT = "}"
+        private const val PATH_PARAM_BOTH = PATH_PARAM_LEFT + PATH_PARAM_RIGHT
+
+        /**
+         * Add standard pagination params to an URL. Uses the same format as many other libraries like Spring. <br/>
+         * An example would be: initialUrl?page=1&size=10&sort=createdAt,desc&sort=name,asc . <br/>
+         * Yes, you can add multiple sort params, each with its own order (direction)
+         */
         @JvmStatic
         fun addPaginationParams(initialUrl: String, pageable: PageableVO): String {
             val finalUrl = addParam(initialUrl, PAGE_PARAM, pageable.page.toString())
@@ -65,9 +74,9 @@ class UrlAcolyte {
 
         @JvmStatic
         fun safeConcat(initialUrl: String, vararg paths: String): String {
-            var url = initialUrl
-            paths.forEach { url = safeConcat(url, it) }
-            return url
+            var trimmedUrl = initialUrl.trim()
+            paths.forEach { trimmedUrl = safeConcat(trimmedUrl, it) }
+            return trimmedUrl
         }
 
         @JvmStatic
@@ -79,6 +88,63 @@ class UrlAcolyte {
             else if (trimmedUrl.endsWith(SLASH) || trimmedPath.startsWith(SLASH))
                 trimmedUrl.plus(trimmedPath)
             else trimmedUrl.plus(SLASH).plus(trimmedPath)
+        }
+
+        /**
+         * Add dynamic path parameters in your URL. All you need is to use {} in your url where you want a path param value to be inserted. <br/>
+         * Example for inserting an API version and user ID dynamically in the URL: http://www.example.com/api/{}/user/{}/friends <br/>
+         * Now you just need to send the param values in order as either a list or array.
+         * You can also replace path params by using an actual param name, this way you don't have to send the values in order.
+         * For this check: {UrlAcolyte.replacePathParamsByName}
+         */
+        @JvmStatic
+        fun addPathParams(initialUrl: String, vararg paramValues: String): String {
+            var trimmedUrl = initialUrl.trim()
+            paramValues.forEach { trimmedUrl = trimmedUrl.replaceFirst(PATH_PARAM_BOTH, it) }
+            return trimmedUrl
+        }
+
+        /**
+         * Add dynamic path parameters in your URL. All you need is to use {} in your url where you want a path param value to be inserted. <br/>
+         * Example for inserting an API version and user ID dynamically in the URL: http://www.example.com/api/{}/user/{}/friends <br/>
+         * Now you just need to send the param values in order as either a list or array.
+         * You can also replace path params by using an actual param name, this way you don't have to send the values in order.
+         * For this check: {UrlAcolyte.replacePathParamsByName}
+         */
+        @JvmStatic
+        fun addPathParams(initialUrl: String, paramValues: List<String>): String {
+            var trimmedUrl = initialUrl.trim()
+            paramValues.forEach { trimmedUrl = trimmedUrl.replaceFirst(PATH_PARAM_BOTH, it) }
+            return trimmedUrl
+        }
+
+
+        /**
+         * Replace named dynamic path parameters in your URL. <br/>
+         * Example for inserting an API version and user ID dynamically in the URL: http://www.example.com/api/{apiVersion}/user/{userId}/friends <br/>
+         * Now you just need to send a map of params where the key is the param's name and the value is its value.
+         * You can also add path params without using actual param names, just an identifier.
+         * For this check: {UrlAcolyte.addPathParams}
+         */
+        @JvmStatic
+        fun replacePathParamsByName(initialUrl: String, params: Map<String, String>): String {
+            var trimmedUrl = initialUrl.trim()
+            params.forEach { trimmedUrl = trimmedUrl.replace(PATH_PARAM_LEFT + it.key + PATH_PARAM_RIGHT, it.value.trim(), ignoreCase = true) }
+            return trimmedUrl
+        }
+
+        /**
+         * Replace named dynamic path parameters in your URL. <br/>
+         * Example for inserting an API version and user ID dynamically in the URL: http://www.example.com/api/{apiVersion}/user/{userId}/friends <br/>
+         * Now you just need to send an array (varargs) of Pair params where the key is the param's name and the value is its value.
+         * You can also add path params without using actual param names, just an identifier.
+         * For this check: {UrlAcolyte.addPathParams}
+         */
+        @JvmStatic
+        fun replacePathParamsByName(initialUrl: String, vararg params: lindar.acolyte.vo.Pair<String, String>): String {
+            var trimmedUrl = initialUrl.trim()
+            params.forEach { trimmedUrl = trimmedUrl.replace(PATH_PARAM_LEFT + it.key + PATH_PARAM_RIGHT, it.value.trim(), ignoreCase = true) }
+            return trimmedUrl
         }
 
         private fun buildSortPath(sort: SortVO): String {
