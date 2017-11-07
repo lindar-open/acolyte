03ec112728ae4671c841418b15f37b40b398b191
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-11-07 23:20:33 +0000

    add boolean extensions

--- src/main/kotlin/lindar/acolyte/extensions/boolext.kt
+++ src/main/kotlin/lindar/acolyte/extensions/boolext.kt
@@ -8,3 +8,22 @@ fun Boolean?.isTrue(): Boolean {
 fun Boolean?.isNotTrue(): Boolean {
     return this == null || this == false
 }
+
+fun Boolean?.ifTrue(action: () -> Unit): Boolean? {
+    if (this.isTrue()) action()
+    return this
+}
+
+fun Boolean?.elseIfTrue(action: () -> Unit): Boolean? {
+    return this.ifTrue(action)
+}
+
+fun Boolean?.ifNotTrue(action: () -> Unit): Boolean? {
+    if (this.isNotTrue()) action()
+    return this
+}
+
+fun Boolean?.elseIfNotTrue(action: () -> Unit): Boolean? {
+    return this.ifNotTrue(action)
+}
+
