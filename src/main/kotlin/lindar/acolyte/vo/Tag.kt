765ba68d69106fba9bc720b7e77e46ffb235ef95
Unknown <cicorongho@yahoo.com>
2018-05-29 17:06:52 +0100

    improve Tag by adding tooltip and action support

--- src/main/kotlin/lindar/acolyte/vo/Tag.kt
+++ src/main/kotlin/lindar/acolyte/vo/Tag.kt
@@ -2,45 +2,59 @@ package lindar.acolyte.vo
 
 import java.io.Serializable
 
-data class Tag (
+data class Tag(
         val text: String,
         val color: String,
         val size: String,
         val shape: String,
-        val hasArrow: Boolean
-) : Serializable {
+        val hasArrow: Boolean,
+        val tooltip: String,
+        val actionTrigger: String
+              ) : Serializable {
     companion object Builder {
-            private var text = ""
-            private var color = ""
-            private var size = ""
-            private var shape = ""
-            private var hasArrow = false
+        private var text = ""
+        private var color = ""
+        private var size = ""
+        private var shape = ""
+        private var hasArrow = false
+        private var tooltip = ""
+        private var action = ""
 
-            fun text(text: String): Tag.Builder {
-                this.text = text
-                return this
-            }
+        fun text(text: String): Tag.Builder {
+            this.text = text
+            return this
+        }
 
-            fun color(color: String): Tag.Builder {
-                this.color = color
-                return this
-            }
+        fun color(color: String): Tag.Builder {
+            this.color = color
+            return this
+        }
 
-            fun size(size: String): Tag.Builder {
-                this.size = size
-                return this
-            }
+        fun size(size: String): Tag.Builder {
+            this.size = size
+            return this
+        }
 
-            fun shape(shape: String): Tag.Builder {
-                this.shape = shape
-                return this
-            }
+        fun shape(shape: String): Tag.Builder {
+            this.shape = shape
+            return this
+        }
 
-            fun hasArrow(hasArrow: Boolean): Tag.Builder {
-                this.hasArrow = hasArrow
-                return this
-            }
+        fun hasArrow(hasArrow: Boolean): Tag.Builder {
+            this.hasArrow = hasArrow
+            return this
+        }
 
-            fun build() = Tag(text, color, size, shape, hasArrow)
+        fun tooltip(tooltip: String): Tag.Builder {
+            this.tooltip = tooltip
+            return this
+        }
+
+        fun action(action: String): Tag.Builder {
+            this.action = action
+            return this
+        }
+
+        fun build() = Tag(text, color, size, shape, hasArrow, tooltip, action)
     }
 }
