1e369e3d14916c88f261ca96d22a7fe0334ba3c1
Unknown <cicorongho@yahoo.com>
2018-06-29 22:01:49 +0100

    allow tag attributes to be null

--- src/main/kotlin/lindar/acolyte/vo/Tag.kt
+++ src/main/kotlin/lindar/acolyte/vo/Tag.kt
@@ -3,13 +3,13 @@ package lindar.acolyte.vo
 import java.io.Serializable
 
 data class Tag(
-        val text: String,
-        val color: String,
-        val size: String,
-        val shape: String,
+        val text: String?,
+        val color: String?,
+        val size: String?,
+        val shape: String?,
         val hasArrow: Boolean,
-        val tooltip: String,
-        val actionTrigger: String
+        val tooltip: String?,
+        val actionTrigger: String?
               ) : Serializable {
     companion object Builder {
         private var text = ""
@@ -20,38 +20,38 @@ data class Tag(
         private var tooltip = ""
         private var action = ""
 
-        fun text(text: String): Tag.Builder {
-            this.text = text
+        fun text(text: String?): Tag.Builder {
+            this.text = text.orEmpty()
             return this
         }
 
-        fun color(color: String): Tag.Builder {
-            this.color = color
+        fun color(color: String?): Tag.Builder {
+            this.color = color.orEmpty()
             return this
         }
 
-        fun size(size: String): Tag.Builder {
-            this.size = size
+        fun size(size: String?): Tag.Builder {
+            this.size = size.orEmpty()
             return this
         }
 
-        fun shape(shape: String): Tag.Builder {
-            this.shape = shape
+        fun shape(shape: String?): Tag.Builder {
+            this.shape = shape.orEmpty()
             return this
         }
 
-        fun hasArrow(hasArrow: Boolean): Tag.Builder {
-            this.hasArrow = hasArrow
+        fun hasArrow(hasArrow: Boolean?): Tag.Builder {
+            this.hasArrow = hasArrow ?: false
             return this
         }
 
-        fun tooltip(tooltip: String): Tag.Builder {
-            this.tooltip = tooltip
+        fun tooltip(tooltip: String?): Tag.Builder {
+            this.tooltip = tooltip.orEmpty()
             return this
         }
 
-        fun action(action: String): Tag.Builder {
-            this.action = action
+        fun action(action: String?): Tag.Builder {
+            this.action = action.orEmpty()
             return this
         }
 
