322b87b84633750a009c32a365c83975382014fb
Ionut <ion33@users.noreply.github.com>
2023-01-11 14:26:03 +0200

    feat: add approximate duration (#12)
    * feat: add approximate duration
    
    * fix typo and reuse variables
    
    * Use variables instead of calculation
    
    Co-authored-by: ionut <ionu@lindar.com>

---
+++ src/main/kotlin/lindar/acolyte/vo/ApproximateDuration.kt
@@ -0,0 +1,25 @@
+package lindar.acolyte.vo
+
+import java.time.temporal.ChronoUnit
+
+data class ApproximateDuration(val duration: Int, val unit: ChronoUnit) {
+
+    fun getDurationFormatted(): String {
+        return when (unit) {
+            ChronoUnit.YEARS -> format(singlePlural(duration, "year", "years"))
+            ChronoUnit.MONTHS -> format(singlePlural(duration, "month", "months"))
+            ChronoUnit.DAYS -> format(singlePlural(duration, "day", "days"))
+            ChronoUnit.HOURS -> format(singlePlural(duration, "hour", "hours"))
+            ChronoUnit.MINUTES -> format(singlePlural(duration, "minute", "minutes"))
+            else -> format(singlePlural(duration, "second", "seconds"))
+        }
+    }
+
+    private fun singlePlural(count: Int, singular: String, plural: String): String {
+        return if (count == 1) singular else plural
+    }
+
+    private fun format(ending: String): String {
+        return "$duration $ending"
+    }
+}
--- src/test/kotlin/lindar/acolyte/util/dates/DateAcolyteTest.kt
+++ src/test/kotlin/lindar/acolyte/util/dates/DateAcolyteTest.kt
@@ -1,12 +1,14 @@
 package lindar.acolyte.util.dates
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.mockStatic
 import java.time.*
+import java.time.temporal.ChronoUnit
 import java.util.stream.Stream
 
 internal class DateAcolyteTest {
@@ -66,7 +68,7 @@ internal class DateAcolyteTest {
             ZonedDateTime.parse(expected),
             ZonedDateTime.parse(currentDate)
                 .with(DateAcolyte.previousOrSameHour(hourOfDay))
-        )
+                    )
     }
 
     companion object {
@@ -81,7 +83,7 @@ internal class DateAcolyteTest {
                 Arguments.of("2022-11-07T10:00:00Z", 10, date, ZoneOffset.UTC),
                 Arguments.of("2022-11-07T22:00:00Z", 22, date, ZoneOffset.UTC),
                 Arguments.of("2022-10-29T22:00:00Z", 22, dateDuringDST, ZoneId.of("Europe/London")),
-            )
+                            )
         }
 
         @JvmStatic
@@ -93,7 +95,7 @@ internal class DateAcolyteTest {
                 Arguments.of("2022-11-09T08:59:59.999999999Z", 9, date, ZoneOffset.UTC),
                 Arguments.of("2022-11-08T09:59:59.999999999Z", 10, date, ZoneOffset.UTC),
                 Arguments.of("2022-11-08T21:59:59.999999999Z", 22, date, ZoneOffset.UTC),
-            )
+                            )
         }
 
         @JvmStatic
@@ -117,7 +119,7 @@ internal class DateAcolyteTest {
                 Arguments.of("2022-10-31T09:00:00Z", 9, date3, ZoneOffset.UTC),
                 Arguments.of("2022-10-24T10:00:00Z", 10, date3, ZoneOffset.UTC),
                 Arguments.of("2022-10-24T22:00:00Z", 22, date3, ZoneOffset.UTC)
-            )
+                            )
         }
 
         @JvmStatic
@@ -141,7 +143,7 @@ internal class DateAcolyteTest {
                 Arguments.of("2022-11-21T08:59:59.999999999Z", 9, date3, ZoneOffset.UTC),
                 Arguments.of("2022-11-14T09:59:59.999999999Z", 10, date3, ZoneOffset.UTC),
                 Arguments.of("2022-11-14T21:59:59.999999999Z", 22, date3, ZoneOffset.UTC),
-            )
+                            )
         }
 
         @JvmStatic
@@ -165,7 +167,7 @@ internal class DateAcolyteTest {
                 Arguments.of("2022-10-01T09:00:00Z", 9, date3, ZoneOffset.UTC),
                 Arguments.of("2022-09-01T10:00:00Z", 10, date3, ZoneOffset.UTC),
                 Arguments.of("2022-09-01T22:00:00Z", 22, date3, ZoneOffset.UTC)
-            )
+                            )
         }
 
         @JvmStatic
@@ -189,7 +191,7 @@ internal class DateAcolyteTest {
                 Arguments.of("2023-01-01T08:59:59.999999999Z", 9, date3, ZoneOffset.UTC),
                 Arguments.of("2022-12-01T09:59:59.999999999Z", 10, date3, ZoneOffset.UTC),
                 Arguments.of("2022-12-01T21:59:59.999999999Z", 22, date3, ZoneOffset.UTC),
-            )
+                            )
         }
 
         @JvmStatic
@@ -200,9 +202,141 @@ internal class DateAcolyteTest {
                 Arguments.of("2022-11-14T14:55:00Z", 14, "2022-11-14T15:55:00Z"),
                 Arguments.of("2022-11-13T14:55:00Z", 14, "2022-11-14T00:55:00Z"),
                 Arguments.of("2022-10-31T14:55:00Z", 14, "2022-11-01T00:55:00Z"),
-            )
+                            )
 
         }
+    }
+
+    @Test
+    fun testApproximateDurationYears() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2023-12-07T00:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(1, result.duration)
+        assertEquals(ChronoUnit.YEARS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationYearsRoundToUpperUnit() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2024-07-07T00:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(1, result.duration)
+        assertEquals(ChronoUnit.YEARS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationYearsRoundToUpperUnitLessThen6Months() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2024-03-07T00:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(1, result.duration)
+        assertEquals(ChronoUnit.YEARS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationMonths() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2023-12-06T00:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(11, result.duration)
+        assertEquals(ChronoUnit.MONTHS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationMonthsRoundToUpperUnitLessThen15Days() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2023-11-15T00:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(11, result.duration)
+        assertEquals(ChronoUnit.MONTHS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationMonthsRoundToUpperUnit() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2023-01-25T00:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(1, result.duration)
+        assertEquals(ChronoUnit.MONTHS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationDays() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2023-01-01T00:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(25, result.duration)
+        assertEquals(ChronoUnit.DAYS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationDaysToUpperUnit() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2023-01-01T23:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(25, result.duration)
+        assertEquals(ChronoUnit.DAYS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationHours() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2022-12-07T05:50:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(5, result.duration)
+        assertEquals(ChronoUnit.HOURS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationMinutes() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2022-12-07T00:50:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(50, result.duration)
+        assertEquals(ChronoUnit.MINUTES, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationSeconds() {
+        val date1 = Instant.parse("2022-12-07T00:00:00.00Z")
+        val date2 = Instant.parse("2022-12-07T00:00:44.50Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(44, result.duration)
+        assertEquals(ChronoUnit.SECONDS, result.unit)
+    }
+
+    @Test
+    fun testApproximateDurationZero() {
+        val date1 = Instant.parse("2022-12-07T00:00:44.50Z")
+        val date2 = Instant.parse("2022-12-07T00:00:00.00Z")
+
+        val result = DateAcolyte.withLondonTimezone().getApproximateDurationBetween(date1, date2);
+
+        assertEquals(0, result.duration)
+        assertEquals(ChronoUnit.MINUTES, result.unit)
     }
 
 
